958416337ffb98295b2a476570ad874e75b9590d
KenshiroKosuke <mahasamud555@gmail.com>
2023-02-14 10:21:30 +0700

    feat(hospitals): create hospital model

--- controllers/hospitals.js
+++ controllers/hospitals.js
@@ -1,20 +1,58 @@
+//const Hospital = require("../models/Hospital.js");
+
+import { Hospital } from "../models/Hospital.js";
+
 //@access public
-export function getHospitals(req, res, next) {
-    res.status(200).json({success:true,msg:'Show all hospital'});
+export async function getHospitals(req, res, next) {
+    try{
+        const hospitals = await Hospital.find();
+        res.status(200).json({ success: true, count: hospitals.length, data: hospitals });
+    } catch(err) {
+        res.status(400).json({ success: false})
+    }
 }
 //@access public
-export function getHospital(req, res, next) {
-    res.status(200).json({succes:true, msg:`Show hospital ${req.params.id}`});
+export async function getHospital(req, res, next) {
+    try{
+        const hospital = await Hospital.findById(req.params.id);
+        if(!hospital){
+            res.status(400).json({ success: false})
+        }
+        res.status(200).json({ success: true, data: hospital });
+    } catch(err) {
+        res.status(400).json({ success: false})
+    }
 }
 //@access private
-export function createHospital(req, res, next) {
-    res.status(200).json({success:true, msg:'Create new hospitals'})
+export async function createHospital(req, res, next) {
+    console.log(req.body)
+    const hospital = await Hospital.create(req.body);
+    res.status(201).json({ success: true, data: hospital })
 }
 //@access private
-export function updateHospital(req, res, next) {
-    res.status(200).json({success:true, msg:`Update hospital id: ${req.params.id}`})
+export async function updateHospital(req, res, next) {
+    try{
+        const hospital = await Hospital.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        });
+        if(!hospital){
+            res.status(400).json({ success: false})
+        }
+        res.status(200).json({ success: true, data: hospital });
+    } catch(err) {
+        res.status(400).json({ success: false})
+    }
 }
 //@access private
-export function deleteHospital(req, res, next) {
-    res.status(200).json({success:true, msg:`Delete hospital id: ${req.params.id}`})
+export async function deleteHospital(req, res, next) {
+    try{
+        const hospital = await Hospital.findByIdAndDelete(req.params.id);
+        if(!hospital){
+            res.status(400).json({ success: false})
+        }
+        res.status(200).json({ success: true, msg: `Delete hospital id: ${req.params.id}`, data: hospital })
+    } catch(err) {
+        res.status(400).json({ success: false})
+    }
 }
